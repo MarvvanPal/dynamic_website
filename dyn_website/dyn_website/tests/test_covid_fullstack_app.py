b351c6d8f45793ebc531efb4e806dd93c4a54f49
Marv van Pal <marvvanpal@gmail.com>
2021-04-23 19:04:07 +0200

    covid -> dyn_website 2

--- dyn_website/dyn_website/tests/test_covid_fullstack_app.py
+++ dyn_website/dyn_website/tests/test_covid_fullstack_app.py
@@ -1,7 +1,7 @@
-from covid_full_stack_app.website import app
+from dyn_website.website import app
 
 # add 2nd level of directory hierarchy so tests run sucessfully from root.
-app.config['DATABASE_FILE'] = 'covid_full_stack_app/' + app.config['DATABASE_FILE']  # noqa: E501
+app.config['DATABASE_FILE'] = 'dyn_website/' + app.config['DATABASE_FILE']  # noqa: E501
 
 
 # a functional test
