b0aaaff672b8129874a11add9f426e4c985c342e
Marv van Pal <marvvanpal@gmail.com>
2021-05-09 22:02:19 +0200

    improving testing

--- dynamic_website/tests/test_dynamic_website.py
+++ dynamic_website/tests/test_dynamic_website.py
@@ -1,6 +1,5 @@
 from dynamic_website.website import app
 
-
 def test_index():
     # create a version of our website that we can use for testing
     with app.test_client() as test_client:
@@ -15,3 +14,19 @@ def test_index():
         html_content = response.data.decode()
 
         assert "<!DOCTYPE html>" in html_content
+
+def about_us():
+    # create a version of our website that we can use for testing
+    with app.test_client() as test_client:
+        # mimic a browser: 'GET /', as if you visit the site
+        response = test_client.get('/about_us')
+
+        # check that the HTTP response is a success
+        assert response.status_code == 200
+
+        # Store the contents of the html response in a local variable.
+        # This should be a string with the same content as the file index.html
+        html_content = response.data.decode()
+
+        assert "<!DOCTYPE html>" in html_content
+
